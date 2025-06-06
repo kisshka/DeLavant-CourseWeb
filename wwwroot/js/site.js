240c84b9addb6fcc4b8afdf3239c974122d8e756
nikolas-p <nikolajpereselkov67@gmail.com>
2025-06-06 12:22:54 +0300

    6 june 2025, 12:22

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -50,7 +50,12 @@ $(document).ready(function () {
         });
     });
     $('input[name="accessType"][value="Common"]').trigger('change');
+    //для того какой выбран нужно radio-container задать border 1px solid red
 });
+
+
+
+
 function autoResize(elem) {
     elem.style.height = 'auto';
     elem.style.height = (elem.scrollHeight-7) + 'px';
@@ -74,3 +79,21 @@ document.querySelectorAll(".menu-item").forEach((item) => {
           }%)`;
         });
       });
+
+
+
+      const select = document.getElementById('post-list');
+
+// Добавляем обработчик события наведения
+select.addEventListener('mouseover', function (event) {
+    if (event.target.tagName === 'OPTION') {
+        event.target.classList.add('hovered'); // Добавляем класс при наведении
+    }
+});
+
+// Добавляем обработчик события ухода курсора
+select.addEventListener('mouseout', function (event) {
+    if (event.target.tagName === 'OPTION') {
+        event.target.classList.remove('hovered'); // Убираем класс при уходе
+    }
+});
